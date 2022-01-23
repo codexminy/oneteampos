1b0456d79513171b5b8cc93087334a845feaeda0
codexminy <codexminy@gmail.com>
2022-01-24 02:12:06 +0900

    시계 수정

--- src/oneteampos/datamodel/Clock.java
+++ src/oneteampos/datamodel/Clock.java
@@ -13,72 +13,70 @@ public class Clock extends JLabel{
 	
 	
 	private boolean aa = true;
-	 private Thread t1;
-	    
-	
-	    private JLabel lblCenter;
+	private Thread t1;
 
-	    Font font;
-	    Color color = (Color.WHITE);
-	    
-	    public void ThreadTime() {
-	    	 t1 = new Thread() {
-	             public void run() {
-	                 
-	                 while(aa) {
-	                 Calendar cal = Calendar.getInstance();
-	                     
-	                     // String으로 객체를 생성하는것보다
-	                     // 메모리를 더 효율적으로 사용하기 위해서 StirngBuffer 사용
-	                     
-	                     StringBuffer now = new StringBuffer();
-	                     
-	                     now.append(cal.get(Calendar.HOUR_OF_DAY));
-	                     now.append("시");
-	                     now.append(cal.get(Calendar.MINUTE));                    
-	                     now.append("분");
-	                     now.append(cal.get(Calendar.SECOND));
-	                     now.append("초");
-	              
-	                     
-	                    setText(now.toString());
-	                 
-	                 try {
-	                     
-	                         Thread.sleep(1000);
-	                         
-	                 } catch(InterruptedException ie) {
-	                     
-	                     ie.printStackTrace();
-	                 
-	                     aa = false;
-	                     
-	                 }
-	             }
-	                 
-	             }
-	         };
-	         t1.start();
-	    }
+	private JLabel lblCenter;
 
-public Clock() {
-    
-    font = new Font("나눔스퀘어",Font.PLAIN,17);
-    
-    // 시계 정보를 담을 JLabel
-    
-    setFont(font);
-    setOpaque(true); 
-    setBackground(color);
+	Font font;
+	Color color = (Color.WHITE);
 
- 
-    
-    // 실행과 동시에 메소드로 지정한 스레드 시작
-    ThreadTime();
- 
-    setBounds(200, 13, 120, 40);
-    setVisible(true);
-    
-}
-	    
+	public void ThreadTime() {
+		t1 = new Thread() {
+			public void run() {
+
+				while (aa) {
+					Calendar cal = Calendar.getInstance();
+
+					// String으로 객체를 생성하는것보다
+					// 메모리를 더 효율적으로 사용하기 위해서 StirngBuffer 사용
+
+					StringBuffer now = new StringBuffer();
+
+					now.append(cal.get(Calendar.YEAR));
+					now.append("년");
+					now.append(cal.get(Calendar.MONDAY) + 1);
+					now.append("월");
+					now.append(cal.get(Calendar.DATE));
+					now.append("일");
+					now.append(cal.get(Calendar.HOUR_OF_DAY));
+					now.append("시");
+					now.append(cal.get(Calendar.MINUTE));
+					now.append("분");
+					now.append(cal.get(Calendar.SECOND));
+					now.append("초");
+
+					setText(now.toString());
+
+					try {
+
+						Thread.sleep(1000);
+
+					} catch (InterruptedException ie) {
+
+						ie.printStackTrace();
+
+						aa = false;
+
+					}
+				}
+
+			}
+		};
+		t1.start();
+	}
+
+	public Clock() {
+
+		font = new Font("나눔스퀘어", Font.PLAIN, 17);
+
+		setFont(font);
+		setOpaque(true);
+		setBackground(color);
+		// 실행과 동시에 메소드로 지정한 스레드 시작
+		ThreadTime();
+		setBounds(200, 13, 300, 40);
+		setVisible(true);
+
+	}
+
 }
